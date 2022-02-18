e215ff43d591fb84c8409335cd79b0c02c5d7ec0
Stanislav Kramar <stanislav.k@gowombat.team>
2022-02-18 10:52:25 +0200

    add comparison functions and operators

--- KeyValuePair.cs
+++ KeyValuePair.cs
@@ -16,4 +16,35 @@ public class KeyValuePair<TKey, TValue>
         Value = value;
     }
 
+    public bool Equals(KeyValuePair<TKey, TValue> other)
+    {
+        return Key.Equals(other.Key);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is KeyValuePair<TKey, TValue> keyValue && Equals(keyValue);
+    }
+
+    public override int GetHashCode()
+    {
+        return Key.GetHashCode();
+    }
+
+    public static bool operator ==(KeyValuePair<TKey, TValue>? obj1, KeyValuePair<TKey, TValue>? obj2)
+    {
+        if (ReferenceEquals(obj1, obj2))
+            return true;
+        if (ReferenceEquals(obj1, null))
+            return false;
+        if (ReferenceEquals(obj2, null))
+            return false;
+
+        return obj1.Equals(obj2);
+    }
+
+    public static bool operator !=(KeyValuePair<TKey, TValue> obj1, KeyValuePair<TKey, TValue> obj2)
+    {
+        return !(obj1 == obj2);
+    }
 }
