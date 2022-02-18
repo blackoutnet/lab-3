e67ca474343255cddf7580750173848e2b3de73e
Stanislav Kramar <stanislav.k@gowombat.team>
2022-02-18 10:54:33 +0200

    implement doubly linked list with basic operations

---
+++ DoublyLinkedList.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+
+namespace CustomHashTable;
+
+public class DoublyLinkedList<TData> : IEnumerable<TData>
+{
+    private DoublyLinkedListNode<TData>? _head;
+    private int _count;
+
+    public int Count => _count;
+
+    public DoublyLinkedList()
+    {
+        _head = null;
+    }
+
+    public void Add(TData data)
+    {
+        var newNode = new DoublyLinkedListNode<TData>(data);
+        if (_head == null)
+        {
+            _head = newNode;
+        }
+        else
+        {
+            _head.Previous = newNode;
+            newNode.Next = _head;
+            _head = newNode;
+        }
+
+        _count++;
+    }
+
+    public void Remove(TData data)
+    {
+        var node = Find(data);
+        if (node != null)
+        {
+            if (node.Next != null)
+            {
+                node.Data = node.Next.Data;
+                node.Next = node.Next.Next;
+            }
+            else if (node.Previous != null)
+            {
+                node.Data = node.Previous.Data;
+                node.Previous = node.Previous.Next;
+            }
+            else
+            {
+                _head = null;
+            }
+        }
+
+        _count++;
+    }
+
+    public DoublyLinkedListNode<TData>? Find(TData data)
+    {
+        var node = _head;
+        if (_head == null)
+        {
+            return null;
+        }
+
+        while (node != null)
+        {
+            if (data.Equals(node.Data))
+            {
+                return node;
+            }
+
+            node = node.Next;
+        }
+
+        return null;
+    }
+}
